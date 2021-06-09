8a5341b67d51450b298c9251f753c32361de7366
dssecret <dsstudios.spam@gmail.com>
2021-06-09 13:17:57 -0700

    Replaced bot license headers

--- bot/dbutils.py
+++ bot/dbutils.py
@@ -1,18 +1,17 @@
-# This file is part of torn-bot.
+# This file is part of torn-command.
 #
-# torn-bot is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of
-# the License, or (at your option) any later version.
+# torn-command is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# torn-bot is distributed in the hope that it will be useful,
+# torn-command is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with torn-bot.  If not, see <https://www.gnu.org/licenses/>.
-
+# You should have received a copy of the GNU Affero General Public License
+# along with torn-command.  If not, see <https://www.gnu.org/licenses/>.
 import json
 
 jsons = ["guilds", "vault", "users", "requests", "armory"]
