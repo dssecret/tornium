914a9fef9db72f7f6357d7d4f6a4585c97317336
dssecret <dsstudios.spam@gmail.com>
2021-07-08 10:05:44 -0700

    Added alembic

---
+++ alembic/versions/d3624cdd6034_initial_revision.py
@@ -0,0 +1,24 @@
+"""Initial Revision
+
+Revision ID: d3624cdd6034
+Revises: 
+Create Date: 2021-07-08 09:43:54.827747
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd3624cdd6034'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
