455f2a6cfe5f5d0732c6a8c2141e31dbbb5f9689
dssecret <dsstudios.spam@gmail.com>
2021-09-27 14:52:32 -0700

    Added API key routes for Lazy Loading
    (WIP) - Requires testing on methods

--- controllers/api/__init__.py
+++ controllers/api/__init__.py
@@ -15,4 +15,9 @@
 
 from flask import Blueprint
 
+from controllers.api import key
+
 mod = Blueprint('apiroutes', __name__)
+
+mod.add_url_route('/api/key', view_func=key.test_key, methods=['GET'])
+mod.add_url_route('/api/key', view_func=key.create_key, methods=['POST'])
