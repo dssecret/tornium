5311b5f57775fdd908dbc30fee1b8c2176851314
dssecret <dsstudios.spam@gmail.com>
2021-09-27 15:01:39 -0700

    Added faction banking (Lazy Loading)

--- controllers/api/__init__.py
+++ controllers/api/__init__.py
@@ -15,10 +15,16 @@
 
 from flask import Blueprint
 
+from controllers.api import faction
 from controllers.api import key
 
 mod = Blueprint('apiroutes', __name__)
 
+
+# /api/key
 mod.add_url_route('/api/key', view_func=key.test_key, methods=['GET'])
 mod.add_url_route('/api/key', view_func=key.create_key, methods=['POST'])
 mod.add_url_route('/api/key', view_func=key.delete_key, methods=['DELETE'])
+
+# /api/faction
+mod.add_url_route('/api/faction/banking', view_func=faction.banking_request, methods=['POST'])
