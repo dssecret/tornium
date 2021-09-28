9a3183990cc9afec738c3df56f60b62125180498
dssecret <dsstudios.spam@gmail.com>
2021-09-28 09:59:42 -0700

    Create guild.js

---
+++ static/bot/guild.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('[data-bs-toggle="tooltip"]').tooltip({
+        container: '.list-group'
+    });
+
+    $('#stakeoutcategory').on('keypress', function(e) {
+        if(e.which === 13) {
+            const id = $('#stakeoutcategory').val();
+            const xhttp = new XMLHttpRequest();
+
+            xhttp.onload = function() {
+                window.location.reload();
+            }
+
+            xhttp.open('POST', `/bot/stakeouts/{{ guildid }}/update?action=category&value=${id}`);
+            xhttp.send();
+        }
+    });
+});
