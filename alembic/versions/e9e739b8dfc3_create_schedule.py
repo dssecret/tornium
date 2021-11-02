31b3bb2dcd627d1d9f4c7ef0a8dc4e78dc758548
dssecret <dsstudios.spam@gmail.com>
2021-11-02 16:57:33 -0700

    Fixed migration

--- alembic/versions/e9e739b8dfc3_create_schedule.py
+++ alembic/versions/e9e739b8dfc3_create_schedule.py
@@ -7,6 +7,9 @@ Create Date: 2021-10-05 16:20:17.072635
 """
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy.dialects import mysql
+
+from redisdb import get_redis
 
 
 # revision identifiers, used by Alembic.
@@ -17,11 +20,18 @@ depends_on = None
 
 
 def upgrade():
-    op.create_table(
-        'Schedules',
-        sa.Column('uuid', sa.String, primary_key=True),
-        sa.Column('factiontid', sa.Integer)
-    )
+    if get_redis().get('dev'):
+        op.create_table(
+            'Schedules',
+            sa.Column('uuid', sa.String, primary_key=True),
+            sa.Column('factiontid', sa.Integer)
+        )
+    else:
+        op.create_table(
+            'Schedules',
+            sa.Column('uuid', mysql.TEXT, primary_key=True),
+            sa.Column('factiontid', mysql.INTEGER)
+        )
 
 
 def downgrade():
