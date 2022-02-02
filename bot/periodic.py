9b2c192dea2082889f3e54c85365b1d97ff01e76
dssecret <dsstudios.spam@gmail.com>
2022-02-01 17:38:41 -0800

    Bug fix
    Signed-off-by: dssecret <dsstudios.spam@gmail.com>

--- bot/periodic.py
+++ bot/periodic.py
@@ -39,4 +39,5 @@ class Periodic(commands.Cog):
         if redis.get('honeybotcheckin') is None or redis.get('honeybotcheckin') == '':
             return
 
-        requests.get(f'https://api.honeybadger.io/v1/check_in/{redis.get("honeybotcheckin")}')
+        request = requests.get(f'https://api.honeybadger.io/v1/check_in/{redis.get("honeybotcheckin")}')
+        self.logger.debug(f'Check-in made... HoneyBadger has responded with {request.content}.')
