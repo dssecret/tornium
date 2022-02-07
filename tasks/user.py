882bbc392d0383542835dbdfb3e6b9d3a3eb6eec
dssecret <webmaster@deek.sh>
2022-02-07 14:15:18 -0800

    Added stakeouts

--- tasks/user.py
+++ tasks/user.py
@@ -13,7 +13,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Tornium.  If not, see <https://www.gnu.org/licenses/>.
 
-from cmath import log
 import math
 
 import honeybadger
