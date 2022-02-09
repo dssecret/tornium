c0fba025a8222e359cc184bc72964d4606857f19
dssecret <dsstudios.spam@gmail.com>
2022-02-08 17:05:54 -0800

    Bug fix test
    Signed-off-by: dssecret <dsstudios.spam@gmail.com>

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -215,6 +215,8 @@ def tornget(endpoint, key, tots=0, fromts=0, stat='', session=None, autosleep=Fa
     redis = get_redis()
     redis_key = f'tornium:torn-ratelimit:{key}'
 
+    logger.info(redis_key)
+
     if redis.setnx(redis_key, 100):
         redis.expire(redis_key, 60 - datetime.datetime.utcnow().second)
     if redis.ttl(redis_key) < 0:
